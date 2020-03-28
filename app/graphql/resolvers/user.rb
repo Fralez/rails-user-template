adc26e2d68a5d33a913f1ed907517aee93f94717
Fralez <franco.gonzalez.burgos@gmail.com>
2020-03-28 03:01:45 -0300

    Added multiple gets to user model

---
+++ app/graphql/resolvers/user.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Resolvers
+  class User < Resolvers::Base
+    type Types::UserType, null: true
+
+    argument :id, ID, required: false
+
+    def resolve(id:)
+      # check_authentication!
+      user = ::User.find_by_id(id)
+
+      return nil if user.nil?
+
+      user
+    end
+  end
+end
---
+++ app/graphql/resolvers/users.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Resolvers
+  class Users < Resolvers::Base
+    type [Types::UserType], null: true
+
+    def resolve()
+      # check_authentication!
+      ::User.all
+    end
+  end
+end
